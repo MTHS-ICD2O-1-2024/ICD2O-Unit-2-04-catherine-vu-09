09c437d0eed0cba9e06c975b4286add3df112a49
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-03-22 00:42:36 +0000

    added index

--- js/script.js
+++ js/script.js
@@ -10,19 +10,6 @@
  * This function calculates area of a triangle.
  */
         function calculateArea() {
-          
+
         }
-            const baseTriangle = document.querySelector("base").value;
-            const heightTriangle = document.querySelector("height").value;
-
-
-            
-            let base = document.getElementById('base').value;
-
-            let height = document.getElementById('height').value;
-            if (base > 0 && height > 0) {
-                let area = 0.5 * base * height;
-                parseFloat(document.getElementById('result')).innerText = "Area is: " + area + " cm²";
-            } else {
-                parseFloat(document.getElementById('result')).innerText = "Please enter integers.";
-            }
+        
