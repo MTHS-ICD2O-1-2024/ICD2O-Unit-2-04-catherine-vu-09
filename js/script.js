fca1f7c4a245d7ba69750bf35c14f6ba1a92fb8d
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-03-23 21:22:26 +0000

    added output

--- js/script.js
+++ js/script.js
@@ -9,20 +9,14 @@
 /**
  * This function calculates area of a triangle.
  */
+ // process
 function calculateArea() {
     let base = document.getElementById('base').value;
     let height = document.getElementById('height').value;
     let result = document.getElementById('result');
 
-    if (base > 0 && height > 0) {
-        let area = 0.5 * base * height;
+    // output
+        let area = 2 * base * height;
         result.innerText = "Area is: " + area + " cm²";
-        result.style.color = "#333"; // Default color for result
-    } else {
-        result.innerText = "Please integers.";
-        result.style.color = "red"; // Change text color if input is invalid
-    }
-
-
-}
+    } 
 
